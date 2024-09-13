6963d1c3ee1027568922431aa97630f1ea5a0df1
dadanhdyt <dadanhidyt@gmail.com>
2024-09-13 08:45:37 +0700

    update

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import figlet from "figlet"
 const text = prompt("Ketikan Nama: ");
 const result = await figlet.text(text as string,{
     font : 'Banner3'
-},(text)=>{
-    console.log();
+},(error : any)=>{
+    console.log(error.message);
     
 });
 console.log(result);
